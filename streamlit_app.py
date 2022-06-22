541d697b327f44a92b2cd76f4cd2cc9d2c9efd3b
Marc Díaz <107620298+yandrak15@users.noreply.github.com>
2022-06-22 15:27:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruit_load_list():
           return my_cur.fetchall()
       
 if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["Snowflake"])
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_rows = get_fruit_load_list()
       streamlit.dataframe(my_data_rows)
 
